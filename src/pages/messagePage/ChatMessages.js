e094d6a84dcf6e43b7406a0019dcd5f63487b792
Emmanuel-code <b.emma.j37@outlook.com>
2022-08-30 22:02:44 +0000

    setup logout from redux store and retrieving messages from store

--- src/pages/messagePage/ChatMessages.js
+++ src/pages/messagePage/ChatMessages.js
@@ -2,43 +2,51 @@ import React, { useEffect, useState } from "react";
 import ChatMessage from "../../component/ChatMessage";
 import MessageInput from "../../component/form/MessageInput";
 import './chatMessages.css'
+import { useDispatch } from "react-redux";
+import { logout } from "../../features/userSlice";
+import { useSelector } from "react-redux";
+import { selectMessage } from "../../features/messageSlice";
 
 function ChatMessages() {
-const messages = [
-    {
-      name: "poau",
-      id: 1,
-      text: 'oh'
-    },
-    {
-      name: "nit",
-      id: 2,
+  const dispatch = useDispatch();
+
+  const messages = useSelector((state) => state.message.messages);
+// const messages = [
+//     {
+//       name: "poau",
+//       id: 1,
+//       text: 'oh'
+//     },
+//     {
+//       name: "nit",
+//       id: 2,
     
-text:'i had a dream'
-    },
-    ,
-    {
-      name: "Brian",
-      id: 3,
+// text:'i had a dream'
+//     },
+//     ,
+//     {
+//       name: "Brian",
+//       id: 3,
       
-      text:'did you see my'
-    },
-  ];
+//       text:'did you see my'
+//     },
+//   ];
 
 
   return (
     <div className="chat__container">
-      <div className="header" >
+      <div className="header">
         <h2>CHATAPP</h2>
-      <h3 ><button>logout</button></h3>
+        <h3 onClick={() => dispatch(logout())}>
+          <button>logout</button>
+        </h3>
       </div>
       <div className="chats">
         {messages.map((m) => (
-          <ChatMessage key={m.id} name={m.name} text={m.text}  />
+          <ChatMessage key={m.id} name={m.name} text={m.text} />
         ))}
-
       </div>
-<MessageInput className='bb'/>
+      <MessageInput className="bb" />
     </div>
   );
 }
