d10fc896e103122795d5b76205e69394d0d0f616
Emmanuel-code <b.emma.j37@outlook.com>
2022-08-30 22:06:31 +0000

    dispatch data from message input form to redux store

--- src/component/form/MessageInput.js
+++ src/component/form/MessageInput.js
@@ -1,18 +1,41 @@
-import { Button } from '@material-ui/core';
-import React,{useState} from 'react'
-import './messageInput.css'
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addChat, chat, selectMessage } from "../../features/messageSlice";
+import "./messageInput.css";
+import { selectUser } from "../../features/userSlice";
 function MessageInput() {
-   const [input, setInput] = useState("");
- 
-  
+  const user = useSelector(selectUser);
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  let chatInput = {
+    id: "",
+    name: "",
+    message: "",
+  };
+  const sendMessage = (e) => {
+    e.preventDefault();
+    chatInput = {
+      name: user.name,
+      message: input,
+    };
+
+   
+    setInput("");
+  };
   return (
     <form>
-      <input type="text" value={ input} onChange={(e)=>setInput(e.target.value)} placeholder="Start typing..." />
-      <Button type="submit" className="sendButton">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Start typing..."
+      />
+      <Button type="submit" onClick={sendMessage} className="sendButton">
         Send
       </Button>
     </form>
   );
 }
 
-export default MessageInput
+export default MessageInput;
