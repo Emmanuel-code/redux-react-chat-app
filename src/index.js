b0bc5b13e53611aff6a1372aec208c2868b179ae
Emmanuel-code <b.emma.j37@outlook.com>
2022-08-31 00:33:09 +0000

    implemented redux persist to persist user and messages in browser

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+let persistor = persistStore(store);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
